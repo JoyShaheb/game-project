fba1487dcbca67610b4c6add5652aa6fa5748200
JoyShaheb <istiaque.joy@northsoutth.edu>
2021-02-16 03:53:09 +0600

    xx

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
-import "phaser";
-
 var config = {
-    type: Phaser.AUTO,
-    width: 400,
-    height: 300,
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
   },
-  game = new Phaser.Game(config),
-  preload = () => {
-    this.load.image("sky");
-  },
-  create = () => {},
-  update = () => {};
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("sky", "./src/assets/sky.png");
+  this.load.image("ground", "./src/assets/platform.png");
+  this.load.image("star", "./src/assets/star.png");
+  this.load.image("bomb", "./src/assets/bomb.png");
+  this.load.spritesheet("dude", "./src/assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+function create() {
+  this.add.image(400, 300, "sky");
+  this.add.image(400, 300, "star");
+}
+
+function update() {}
